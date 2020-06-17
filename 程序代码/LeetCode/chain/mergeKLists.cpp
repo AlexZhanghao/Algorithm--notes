f393c14007786adbafc1d39a2a42f58a10981ba0
363040909 <363040909@qq.com>
2020-06-17 22:52:56 +0800

    2020/6/17--notebook

--- 程序代码/LeetCode/chain/mergeKLists.cpp
+++ 程序代码/LeetCode/chain/mergeKLists.cpp
@@ -13,7 +13,58 @@ struct ListNode {
 
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL){
+            return l2;
+        }
+        else if(l2==NULL){
+            return l1;
+        }
+        else if(l1->val<l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+        else{
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+    }
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+        if(lists.size()==0) return nullptr;
+        for(int i=1;i<lists.size();++i){
+            mergeTwoLists(lists[0],lists[i]);
+        }
+
+        return lists[0];
+    }
+};
+
+// 小根堆的回调函数
+struct cmp{  
+    bool operator()(ListNode *a,ListNode *b){
+        return a->val > b->val;
+    }
+};
+
+//优先队列
+class Solution2 {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pri_queue;
+        // 建立大小为k的小根堆
+        for(auto elem : lists){
+            if(elem) pri_queue.push(elem);
+        }
+        // 可以使用哑节点/哨兵节点
+        ListNode dummy(-1);
+        ListNode* p = &dummy;
+        // 开始出队
+        while(!pri_queue.empty()){
+            ListNode* top = pri_queue.top(); pri_queue.pop();
+            p->next = top; p = top;
+            if(top->next) pri_queue.push(top->next);
+        }
+        return dummy.next;  
     }
 };
---
+++ 程序代码/基础/memcpyandmemmove.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+//memcpy和memmove（）都是C语言中的库函数，在头文件string.h中，作用是从src内存中拷贝n个字节到dst内存区域。
+//他们的区别是，当内存发生局部重叠的时候，memmove保证拷贝的结果是正确的，memcpy不保证拷贝的结果的正确。
+void *my_memcpy(void *dst,const void* src,size_t n){
+    char *tmp=(char*)dst;
+    char *s_src=(char*)src;
+
+    while(n){
+        *tmp=*s_src;
+        ++tmp;
+        ++s_src;
+        --n;
+    }
+    return dst;
+}
+
+//memmove应对重叠的策略就是从后往前遍历
+void *my_memmove(void* dst,const void*src,size_t n){
+    char *s_dst=(char*)dst;
+    char *s_src=(char*)src;
+
+    if(s_dst>s_src&&(s_src+n>s_dst)){
+        s_dst=s_dst+n-1;
+        s_src=s_src+n-1;
+        while(n){
+            *s_dst=*s_src;
+            --s_src;
+            --s_dst;
+            --n;
+        }
+    }
+    else{
+        while(n){
+            *s_dst=*s_src;
+            --s_src;
+            --s_dst;
+            --n;
+        }
+    }
+
+    return dst;
+}
